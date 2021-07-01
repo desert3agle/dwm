64e1ce685797bce9114c57533a28f9b3779d4bcb
Harsh Vardhan <harsh.vardhan320@gmail.com>
2021-07-01 21:40:18 +0530

    pywal integration

--- config.def.h
+++ config.def.h
@@ -8,23 +8,26 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int horizpadbar 	    = 0; 	/* horizontal padding for statusbar */
 static const int vertpadbar  	    = 7; 	/* vertical padding for statusbar */
-//static const char *fonts[]        = {"FontAwesome:size=12", "monospace:size=10" };
-static const char dmenufont[]       = "DejaVu Sans:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
-static const char *colors[][3]      = {
-	/*               fg          bg         border  */ 
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
-};
 
+//static const char *fonts[]        = {"FontAwesome:size=12", "monospace:size=10" };
+//static const char dmenufont[]       = "DejaVu Sans:size=10";
+
+// static const char col_gray1[]       = "#222222";
+// static const char col_gray2[]       = "#444444";
+// static const char col_gray3[]       = "#bbbbbb";
+// static const char col_gray4[]       = "#eeeeee";
+// static const char col_cyan[]        = "#005577";
+// static const char *colors[][3]      = {
+// 	/*               fg          bg         border  */ 
+// 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+// 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+// };
+
+#include "/home/khoya_musafir/.cache/wal/colors-wal-dwm.h"
 
 static const char *fonts[]          = { 
 	"DejaVu Sans:size:pixelsize=16:antialias=true:autohint=true",
-     	"Font Awesome 5 Free Solid:size:pixelsize=13:antialias=true:autohint=true"
+    "Font Awesome 5 Free Solid:size:pixelsize=13:antialias=true:autohint=true"
 };
 
 
@@ -68,7 +71,8 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+//static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 #include "movestack.c"
