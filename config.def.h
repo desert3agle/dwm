52f81372b7340b60cab2b45d7259b0060b4dce91
Harsh Vardhan <harsh.vardhan320@gmail.com>
2021-07-25 15:21:12 +0530

    unused windows

--- config.def.h
+++ config.def.h
@@ -35,7 +35,9 @@ static const char *fonts[]          = {
 
 /* tagging */
 //static const char *tags[] = { "1", "2", "3", "4", "5", "6",  "7", "8", "9" };
-static const char *tags[] = { "", "︁", "︁", "︁", "︁", "", "︁", "︁", "︁" };
+//static const char *tags[] = { "", "︁", "︁", "︁", "︁", "", "︁", "︁", "︁" };
+
+static const char *tags[] = { "", "︁", "︁",  "︁" };
 
 static const Rule rules[] = {
 	/* xprop(1):
